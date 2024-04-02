91acc73477aa120c6b3b54ee3e57ee4fa6357b60
azita <azita@email.com>
2024-04-02 15:21:12 +0330

    change test file

--- accounts/test/test_view.py
+++ accounts/test/test_view.py
@@ -72,21 +72,6 @@ class test_user_login_view_without_code(TestCase):
         self.assertRedirects(respone,reverse('home:home'))
 
     
-    # @override_settings(USE_TZ=False)
-    # @mock.patch('accounts.views.Userlogin.now')
-    # def test_login_code_valid_form(self,mock_now):
-    #      mock_now.side_effect=[
-    #          datetime.datetime(2024,1,1,8,2,5)
-    #      ]
-          
-    #      form=logincodeForm(data={'code':'1234'})
-    #      OtpCode.objects.create(ohone_number='09121111111',code='1234',created=datetime.datetime(2024,1,1,8,2,0))
-    #      session = self.client.session
-    #      session['phone_number'] = '09121111111'
-    #      session.save()
-        
-    #      respone=self.client.post(reverse('accounts:login_with_code',{'code_id':'1'}))
-    #      self.assertRedirects(respone,reverse('accounts:login_sms'))
 
         
 
